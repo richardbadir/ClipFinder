5e576ba3d4363f78cd9bf3ca7ce9c380966396ef
Richard Badir <ricobadir@hotmail.com>
2024-01-20 16:01:46 -0500

    Finding time

--- transcript.py
+++ transcript.py
@@ -1,8 +1,9 @@
 from transformers import pipeline
 from youtube_transcript_api import YouTubeTranscriptApi
+from OpenAI import findAnswer, VideoSummary, ExplainAnswer
 
 class Transcript():
-    def __init__(self, link :str) ->str:
+    def __init__(self, link :str):
         #implement a function that would get link from kevin code
         link = "https://www.youtube.com/watch?v=5yh0HA4Mjfc&t=9s"
 
@@ -10,12 +11,26 @@ class Transcript():
         video_id = link.split("=")[1]
 
         YouTubeTranscriptApi.get_transcript(video_id)
-        transcript = YouTubeTranscriptApi.get_transcript(video_id)
-        self.trans = transcript
+        self.transcript = YouTubeTranscriptApi.get_transcript(video_id)
 
-        result=""
+        self.result=""
 
-        for i in transcript:
-            result+= ' ' + i['text']
+        for i in self.transcript:
+            self.result+= ' ' + i['text']
 
-        return result
+    def findTime(self):
+        answers = findAnswer()
+        for answer in answers:
+            answer.strip()
+            answer.lower()
+            answer.strip("\"")
+            if ":" in answer:
+                answer = answer.split(":")[1]
+                answer.strip("\"")
+            if "\"" in answer:
+                answer = answer.split("\"")[1]
+            if answer in self.results:
+                
+
+        
+

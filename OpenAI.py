1dd999a30c559587379735b65f418e239413ec5a
Richard Badir <ricobadir@hotmail.com>
2024-01-20 14:19:27 -0500

    OpenAI ReadY

--- OpenAI.py
+++ OpenAI.py
@@ -1,12 +1,32 @@
 from openai import OpenAI
 client = OpenAI()
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+def findAnswer(transcript: str, question:str):
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": f"Your role is to find and return the exact passage in the transcript where the answer to the user's the question is given. Nothing less, nothing more. Do not surroudn it by any other words or quotations. Here is the transcript: {transcript}"},
+        {"role": "user", "content": f"Here is question to which you need to find the answer: {question}"}
+    ]
+    )
+    return completion.choices
 
-print(completion.choices[0].message)
+def VideoSummary(transcript: str, question:str):
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "Your role is to summarize the transcript."},
+        {"role": "user", "content": f"Transcript: \"{transcript}\""}
+    ]
+    )
+    return completion.choices
+
+def ExplainAnswer(transcript: str, question:str):
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": f"Your role is to find the answer to the user's question based on the transcript. Here is the transcript: {transcript}"},
+        {"role": "user", "content": f"Here is question to which you need to find the answer: \"{question}\""}
+    ]
+    )
+    return completion.choices

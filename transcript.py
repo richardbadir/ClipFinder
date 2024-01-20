6e7f41c05007f5af0f53582e50750508a0b217a6
Richard Badir <ricobadir@hotmail.com>
2024-01-20 15:19:52 -0500

    Keep in memory transcript

--- transcript.py
+++ transcript.py
@@ -1,23 +1,21 @@
 from transformers import pipeline
 from youtube_transcript_api import YouTubeTranscriptApi
 
+def transcript(link :str) ->str:
+    #implement a function that would get link from kevin code
+    link = "https://www.youtube.com/watch?v=5yh0HA4Mjfc&t=9s"
 
-#implement a function that would get link from kevin code
-link = "https://www.youtube.com/watch?v=5yh0HA4Mjfc&t=9s"
+    #getting video ID
+    video_id = link.split("=")[1]
 
-#getting video ID
-video_id = link.split("=")[1]
+    YouTubeTranscriptApi.get_transcript(video_id)
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
 
-YouTubeTranscriptApi.get_transcript(video_id)
-transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    result=""
 
-transcript
+    for i in transcript:
+        result+= ' ' + i['text']
 
-result=""
-
-for i in transcript:
-    result+= ' ' + i['text']
-
-print(result)
+    return result
 
 #summarizer = pipeline('summarization')
